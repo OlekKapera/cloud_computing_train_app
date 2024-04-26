d28edee60c8f5bd1d356bda8b2d9ed1e9fa44dc5
Aleksander Kapera <aleksanderkapera@gmail.com>
2024-04-26 16:12:13 -0400

    Add object modifications in space

--- lib/traffic_light_screen.dart
+++ lib/traffic_light_screen.dart
@@ -1,4 +1,5 @@
 import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
@@ -37,22 +38,27 @@ class TrafficLightScreenState extends State<TrafficLightScreen> {
             setState(() => idle = !idle);
           },
         ),
-        body: Container(
-          child: ARKitSceneView(
-            showFeaturePoints: true,
-            planeDetection: ARPlaneDetection.horizontal,
-            onARKitViewCreated: onARKitViewCreated,
-          ),
+        body: ARKitSceneView(
+          showFeaturePoints: true,
+          enablePanRecognizer: true,
+          enablePinchRecognizer: true,
+          enableRotationRecognizer: true,
+          planeDetection: ARPlaneDetection.horizontal,
+          onARKitViewCreated: onARKitViewCreated,
         ),
       );
 
   void onARKitViewCreated(ARKitController arkitController) {
     this.arkitController = arkitController;
     this.arkitController.onAddNodeForAnchor = _handleAddAnchor;
+    this.arkitController.onNodePinch = (pinch) => _onPinchHandler(pinch);
+    this.arkitController.onNodePan = (pan) => _onPanHandler(pan);
+    // this.arkitController.onNodeRotation =
+    //     (rotation) => _onRotationHandler(rotation);
   }
 
   void _handleAddAnchor(ARKitAnchor anchor) {
-    if (!(anchor is ARKitPlaneAnchor)) {
+    if (anchor is! ARKitPlaneAnchor) {
       return;
     }
     _addPlane(arkitController, anchor);
@@ -68,5 +74,33 @@ class TrafficLightScreenState extends State<TrafficLightScreen> {
       scale: vector.Vector3(0.002, 0.002, 0.002),
     );
     controller?.add(node!, parentNodeName: anchor.nodeName);
+  }
+
+  void _onPinchHandler(List<ARKitNodePinchResult> pinch) {
+    final pinchNode = pinch.firstOrNull;
+    if (pinchNode != null) {
+      final scale = 1 - ((1 - pinchNode.scale) * 0.01);
+      final oldScale = node?.scale ?? vector.Vector3.zero();
+      node?.scale = oldScale * scale;
+    }
+  }
+
+  void _onPanHandler(List<ARKitNodePanResult> pan) {
+    final panNode = pan.firstOrNull;
+    if (panNode != null) {
+      final oldPosition = node?.position;
+      final translation = panNode.translation * 0.001;
+      node?.position = vector.Vector3((oldPosition?.x ?? 0) + translation.x,
+          oldPosition?.y ?? 0, (oldPosition?.z ?? 0) + translation.y);
+    }
+  }
+
+  void _onRotationHandler(List<ARKitNodeRotationResult> rotation) {
+    final rotationNode = rotation.firstOrNull;
+    if (rotationNode != null) {
+      final oldRotation = node?.eulerAngles ?? vector.Vector3.zero();
+      node?.eulerAngles = vector.Vector3(
+          oldRotation.x + rotationNode.rotation, oldRotation.y, oldRotation.z);
+    }
   }
 }
